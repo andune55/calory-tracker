97e786e120e74578fbb540580c8c409418989c43
Agustin <latierramedia@gmail.com>
2025-02-18 14:25:53 +0100

    reducción estilos actividades en rama estilos-fo

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -26,23 +26,26 @@ export default function ActivityList({activities, dispatch}: ActivityListProps) 
             ? <p className="text-center my-5">No hay actividades aún</p> :
         
         activities.map(activity => (
-            <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
+            <div key={activity.id} className="px-5 py-2 bg-white mt-4 flex justify-between">
                 <div className="space-y-2 relative">
                     {/* Muestra actividad: categoría, nombre y calorías */}
-                    <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                    <p className={`absolute -top-6 -left-8 px-5 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
                         {/* {activity.category} */}
                         {categoryName(+activity.category)}
                         
                     </p>
-                    <p className="text-2xl font-bold pt-5">{activity.name}</p>
-                    <p className="font-black text-4xl text-lime-500">
-                        {activity.calories} {''}
-                        <span>Calorías</span>
-                    </p>
+                    <div className="flex items-baseline">
+                        <p className="text-xl font-bold">{activity.name} - </p> 
+                        <p className="font-black text-xl text-lime-500 ml-1.25">
+                            {activity.calories} {''}
+                            <span>Calorías</span>
+                        </p>
+                    </div>
                 </div>
                 <div className="flex gap-5 items-center">
                     {/* Acciones para editar/eliminar esa actividad */}
                     <button
+                        className="cursor-pointer"
                         onClick={() => dispatch({type:"set-activeId", payload: {id: activity.id}})}
                     >
                         <PencilSquareIcon 
@@ -51,6 +54,7 @@ export default function ActivityList({activities, dispatch}: ActivityListProps) 
                     </button>
 
                     <button
+                        className="cursor-pointer"
                         onClick={() => dispatch({type:"delete-activity", payload: {id: activity.id}})}
                     >
                         <XCircleIcon 
